1526a6e3193a02557fbf758d481e19141a66c974
@hexlet/cli <support@hexlet.io>
2025-01-17 16:17:03 +0400

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -8,33 +8,33 @@ class StackTest < Minitest::Test
   # BEGIN
   def test_pushes_elements_to_the_stack
     stack = Stack.new
-    stack.push(1)
-    stack.push(2)
-    stack.push(3)
+    stack.push!(1)
+    stack.push!(2)
+    stack.push!(3)
     assert { stack.to_a == [1, 2, 3] }
   end
 
   def test_pops_elements_from_the_stack
     stack = Stack.new
-    stack.push(1)
-    stack.push(2)
-    stack.push(3)
-    assert { stack.pop == 3 }
+    stack.push!(1)
+    stack.push!(2)
+    stack.push!(3)
+    assert { stack.pop! == 3 }
     assert { stack.to_a == [1, 2] }
   end
 
   def test_should_clear_the_stack
     stack = Stack.new
-    stack.push(1)
-    stack.push(2)
-    stack.clear
+    stack.push!(1)
+    stack.push!(2)
+    stack.clear!
     assert { stack.to_a == [] }
   end
 
   def test_should_check_empty
     stack = Stack.new
     assert { stack.empty? }
-    stack.push(1)
+    stack.push!(1)
     assert { !stack.empty? }
   end
   # END
